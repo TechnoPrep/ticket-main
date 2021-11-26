f4733abbdfe600078ae8b9a949e7cb5b72f40fb1
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-11-25 22:49:03 -0700

    added Auth and dateformatter boilerplates

---
+++ server/utils/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config({path: "../.env"});
+
+const secret = process.env.AUTH_SECRET;
+const expiration = process.env.AUTH_EXP;
+
+module.exports = {
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ email, lastName, _id }) {
+    const payload = { email, lastName, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
