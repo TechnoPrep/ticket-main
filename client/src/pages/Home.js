15a0b5172ed9ce89c1eac826057cb64892190e26
LoopySquare <tjobin1@gmail.com>
2021-11-26 18:30:53 -0400

    added a basic search bar to test api. Added working links to navbar. Statred navbar style.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,8 +8,11 @@ const Home = () => {
   // });
 
   return (
-    <>
-    </>
+    <div className='search-input'>
+      <label for="site-search">Placeholder search for api testing</label>
+      <input className='m-2' type="search" id="site-search" name="q" aria-label="Search through site content"/>
+      <button>Search</button>
+    </div>
   );
 };
 
