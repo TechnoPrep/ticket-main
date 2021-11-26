84ac2d67161d0470cc07671726d6ce32085dea23
kweinerman <85956994+kweinerman@users.noreply.github.com>
2021-11-26 14:26:30 -0700

    Added Userprofile and eventlist component

---
+++ client/src/components/EventList/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const EventList = ({
+  events,
+  title,
+  showTitle = true,
+  showUsername = true,
+}) => {
+  if (!events.length) {
+    return <h3>No events Yet</h3>;
+  }
+
+  return (
+    <div>
+      {showTitle && <h3>{title}</h3>}
+      {events &&
+        events.map((event) => (
+          <div key={event._id} className="card mb-3">
+            <h4 className="card-header bg-primary text-light p-2 m-0">
+              {showUsername ? (
+                <Link
+                  className="text-light"
+                  to={`/profiles/${event.eventAuthor}`}
+                >
+                  {event.eventAuthor} <br />
+                  <span style={{ fontSize: '1rem' }}>
+                    saved this event on {event.createdAt}
+                  </span>
+                </Link>
+              ) : (
+                <>
+                  <span style={{ fontSize: '1rem' }}>
+                    You saved this event on {event.createdAt}
+                  </span>
+                </>
+              )}
+            </h4>
+            <div className="card-body bg-light p-2">
+              <p>{event.eventText}</p>
+            </div>
+            <Link
+              className="btn btn-primary btn-block btn-squared"
+              to={`/events/${event._id}`}
+            >
+              Join the discussion on this event.
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default EventList;
