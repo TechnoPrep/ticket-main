bfa174be90dca2c749877023e0aa0795ec7d915b
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-11-27 13:34:26 -0700

    Updated the resolved and typeDefs to handle account Registration and emailConfirmed field

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -37,6 +37,11 @@ const typeDefs = gql`
     user: User
   }
 
+  type RegAuth {
+    token: ID
+    user: User
+  }
+
   type Query {
     user(email: String!): User
     savedEvents(email: String): [SavedEvent]
@@ -45,8 +50,8 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    addUser(firstName: String!, lastName: String!, phone: String!, zipCode: String!, email: String!, password: String!): Auth
-    accountReg(email: String!, emailConfirmed: Boolean!): Auth
+    addUser(firstName: String!, lastName: String!, phone: String!, zipCode: String!, email: String!, password: String!): Auth!
+    accountReg(token: String!): RegAuth
     eventPrefSetup(email: String!): User
     login(email: String!, password: String!): Auth
     addEvent(eventName: String!, venue: String!, zipCode: Int!, lat: Float!, lon: Float!, eventDate: String!, eventImage: String!): SavedEvent
