18689fe3ae5c71e2012ee5f11d82770d1c14e59b
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-12-02 07:10:01 -0700

    added functionality to handle the event and venue queries

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,113 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
 
-// import {  } from '../utils/queries';
+import { fetchEvents, fetchLocation, fetchVenues } from '../utils/apiQueries'
 
-const Home = () => {
-  // const { loading, data } = useQuery( {
-  //   fetchPolicy: "no-cache"
-  // });
+import Results from '../components/Results'
+
+const Home = ({apitokens}) => {
+  
+  const [eventList, setEventList] = useState([]);
+  
+  const [queryState, setQueryState] = useState({
+    eventName: '',
+    zipCode: '',
+    radius: '',
+    lat: '',
+    lon: '',
+  });
+
+  const [eventQuery, setEventQuery] = useState({
+    venues: [],
+    performers: []
+  })
+
+  useEffect(() => {
+    console.log('useEffect ran');
+  }, [])
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const lat = 39.7316982;
+    const lon = -104.9431649;
+    const radius = 50;
+ 
+    fetchVenues(apitokens, lat, lon, radius)
+        
+    
+    // const results = await fetchEvents(apitokens)
+    // setEventList(results)
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setQueryState({
+      ...queryState,
+      [name]: value,
+    });
+  };
 
   return (
     <div className='home'>
     <div className='search-input'>
+    <form onSubmit={handleFormSubmit}>
+      <label 
+        className='sample-search'
+        htmlFor="eventName">
+          Event / Performer Name
+      </label>
+     
+      <input 
+        className='m-2' 
+        type="text" 
+        name="eventName"
+        value={queryState.eventName}
+        onChange={handleChange}
+        aria-label="Search for tickets"
+        />
 
-      <label className='sample-search'for="site-search">Placeholder search for api testing</label>
+      <label 
+        className='sample-search'
+        htmlFor="zipCode">
+          Zip Code
+      </label>
+     
+      <input 
+        className='m-2' 
+        type="text" 
+        name="zipCode"
+        value={queryState.zipCode}
+        onChange={handleChange}
+        aria-label="Search for tickets"
+        />
 
-      <input className='m-2' type="search" id="site-search" name="q" aria-label="Search through site content"/>
-      <button>Search</button>
+      <label 
+        className='sample-search'
+        htmlFor="Radius">
+          Radius:
+      </label>
+     
+      <select value={queryState.radius} onChange={handleChange}>
+        <option name="10">10</option>
+        <option name="25">25</option>
+        <option name="50">50</option>
+        <option name="100">100</option>
+        <option name="200">200</option>
+      </select>
+
+      <button type="submit">Search</button>
+        </form>
+      </div>
+      <div className="flex-row justify-center mb-3">
+      <div className="col-12 col-md-10 mb-5">
+        <Results 
+          events={eventList}
+          title={`My Searched Events events...`}
+          showTitle={false}
+        />
+      </div>
     </div>
     </div>
   );
