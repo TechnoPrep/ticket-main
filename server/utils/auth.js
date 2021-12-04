94e693299f2c37bd0ad485e613731c54c3f6d4cb
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-12-03 17:33:25 -0700

    added method to validate token has not expired

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const decode = require('jwt-decode');
 
 const path = require('path');
 require('dotenv').config({path: path.join(__dirname, '../.env')})
@@ -29,6 +30,17 @@ module.exports = {
   },
   signToken: function ({ email, firstName, _id }, exp = expiration) {
     const payload = { email, firstName, _id };
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    return jwt.sign({ data: payload }, secret, { expiresIn: exp });
   },
+
+  isTokenExpired: function (token) {
+    // Decode the token to get its expiration time that was set by the server
+    const decoded = decode(token);
+    // If the expiration time is less than the current time (in seconds), the token is expired and we return `true`
+    if (decoded.exp < Date.now() / 1000) {
+      return true;
+    }
+    // If token hasn't passed its expiration time, return `false`
+    return false;
+  }
 };
