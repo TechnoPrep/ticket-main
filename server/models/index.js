94bca8aef112023ee2cd63f994b7b6951178dac4
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-12-04 19:09:13 -0700

    Removed EventPrefs relationships

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,4 @@
 const User = require('./User');
-const EventPref = require('./EventPref');
 const SavedEvent = require('./SavedEvent');
 
-module.exports = { User, EventPref, SavedEvent };
+module.exports = { User, SavedEvent };
