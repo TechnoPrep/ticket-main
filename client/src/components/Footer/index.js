4a28a1f1082fc463e7315536df7ebbd202e2735e
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-12-09 13:44:58 -0700

    Added logic for when you click the "Go Back" button it also handles the displayHero state

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
 
-const Footer = () => {
+const Footer = ({displayHero}) => {
   const location = useLocation();
   const history = useHistory();
+
+  const handleOnClick = () => {
+    displayHero(true)
+    history.goBack()
+  }
+
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
       <div className="container text-center mb-5">
         {location.pathname !== '/' && (
           <button
             className="btn go-back-btn mb-3"
-            onClick={() => history.goBack()}
+            onClick={handleOnClick}
           >
             &larr; Go Back
           </button>
