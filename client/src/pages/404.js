36a34f6d22a6d51d48515e2ac567718a5a8ced29
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-12-09 13:48:00 -0700

    udpated image to be used and text displayed on the page

--- client/src/pages/404.js
+++ client/src/pages/404.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
-import PageNotFoundImg from '../img/404-image.jpg'
+import PageNotFoundImg from '../img/404-Image.png'
 
-function PageNotFound() {
+function PageNotFound({displayHero}) {
+
+  useEffect(() => [
+    displayHero(false)
+  ], [])
 
   return (
     <div className="container"> 
+    <br/>
+    <div className="pagenotefound">
+      <h3 className="pagenotfound-text">PAGE NOT FOUND</h3>
+      <br/>
+    </div>
       <div className="pagenotfound-image">
         <img src={PageNotFoundImg} alt="Bill Murray Caddyshack Meme"/>
-      </div>
+      </div>!
+      <h3 className="pagenotfound-text">But you found this page, so you got that going for you.</h3>
+      <h3 className="pagenotfound-text">Which is nice...</h3>
     </div>
   );
 }
