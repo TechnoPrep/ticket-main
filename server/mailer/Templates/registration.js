a3f50cc9ad41fe24d5745b27cc02423d29648fbc
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-12-08 23:38:30 -0700

    Updated the templates to make them more user friendly

--- server/mailer/Templates/registration.js
+++ server/mailer/Templates/registration.js
@@ -4,13 +4,9 @@ const registration = (url, email) => {
   <br>
   <p style="font-size:20px">Click here to complete your <a href="${url}">account registration</a>.</p>
   <br>
-  <p style="font-size:20px> This email verification is for 
-    <a href="mailto:${email}">
-      ${email}.
-    </a>
-  </p>
+  <p style="font-size:20px">This email verification is for <a href="mailto:${email}">${email}</a>.</p>
   <br>
-  <p style="font-size:20px>If this is not correct, please ignore this email.</p>
+  <p style="font-size:20px">If this is not correct, please ignore this email.</p>
   <br>
   <br>
   <p style="font-size:18px"> If the link above does not work please copy and paste this link into your browser</p>
